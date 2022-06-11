7325ded8860276b0e623faf347b295390ed5098f
Petr Kozelka <pkozelka@gmail.com>
2022-06-11 17:23:56 +0200

    trivial response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use salvo::prelude::*;
+
+#[fn_handler]
+async fn hello_world() -> &'static str {
+    "hello_world"
+}
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().get(hello_world);
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .serve(router).await
 }
