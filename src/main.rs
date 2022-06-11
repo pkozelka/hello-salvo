4010f5e356caa2f3c33a918a735d39e917db5ce1
Petr Kozelka <pkozelka@gmail.com>
2022-06-11 17:59:26 +0200

    serve static file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use salvo::prelude::*;
+use salvo::extra::serve_static::FileHandler;
 
 #[fn_handler]
 async fn hello_world() -> &'static str {
@@ -7,7 +8,12 @@ async fn hello_world() -> &'static str {
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().get(hello_world);
+    // let router = Router::new().get(hello_world);
+    let router = Router::new().push(
+        Router::new()
+        .path("")
+        .get(FileHandler::new("public/index.html"))
+    );
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .serve(router).await
 }
